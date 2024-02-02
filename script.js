53b0aa7989957f01f1cd4296493fbde85fae12aa
aadi1335 <122515467+aadi1335@users.noreply.github.com>
2024-02-02 11:45:47 +0530

    test

---
+++ script.js
@@ -0,0 +1,7 @@
+// toggle menu icon reference
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
